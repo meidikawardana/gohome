2405ba5ba136c29a4dcfb664e8a8c81ced0da136
Meidika Wardana <jdeep555.mw@gmail.com>
2018-11-10 22:41:04 +0700

    first commit

---
+++ app/src/main/java/com/pintar_android/gohome/MainActivity.java
@@ -0,0 +1,25 @@
+package com.pintar_android.gohome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }
+}
